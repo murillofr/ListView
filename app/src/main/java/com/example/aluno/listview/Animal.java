8713ef1db52914f247a209a1d4a601ec6c02658b
murillofr <murillo.falcao@gmail.com>
2017-09-18 17:59:00 -0300

    Segundo Commit

---
+++ app/src/main/java/com/example/aluno/listview/Animal.java
@@ -0,0 +1,10 @@
+package com.example.aluno.listview;
+
+public class Animal {
+    public String nome;
+    public int foto;
+    public Animal(String nome, int foto) {
+        this.nome = nome;
+        this.foto = foto;
+    }
+}
